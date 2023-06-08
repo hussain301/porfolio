2d4c6a9ca68ce25843e737baf10fdc3c5a9c6de4
hussain <hussainnariwal@gmail.com>
2023-06-08 11:11:03 +0500

    sanity cdn to fasle

--- sanity.ts
+++ sanity.ts
@@ -5,7 +5,7 @@ export const config={
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
     projectId: process.env.NEXT_PUBLIC_SANITY_ID!,
     apiVersion: '2023-01-10',
-    useCdn: process.env.NODE_ENV==='production'
+    useCdn: false
 }
 
 export const sanityClient = createClient(config)
